6e92c23cc370d6bd0e814eb415e89111271f4444
destitutiones <albina.rukhadze@gmail.com>
2023-12-01 05:44:13 +0300

    Added pathlib usage

--- mlops_tools/infer.py
+++ mlops_tools/infer.py
@@ -1,21 +1,25 @@
-import os
 import pickle
 import sys
+from pathlib import Path
 
+import hydra
 import pandas as pd
 
 
-example_dir = os.path.dirname(os.path.realpath(__file__))
-sys.path.append(os.path.dirname(example_dir))
+mlops_tools_dir_path = Path(__file__).absolute().parent
+root_dir_path = mlops_tools_dir_path.parent
+sys.path.append(str(mlops_tools_dir_path))
+sys.path.append(str(root_dir_path.joinpath("configs")))
+
+from configs.config import Params  # noqa: E402
+
 from mlops_tools.utils import load_dataset  # noqa: E402
 
 
-MODEL_NAME = "basic_lin_reg"
-
-
-def main():
+@hydra.main(config_path="../configs", config_name="config", version_base="1.3")
+def main(cfg: Params) -> None:
     # Load the model from disk
-    reg = pickle.load(open(MODEL_NAME + ".sav", "rb"))
+    reg = pickle.load(open(cfg["model"]["name"] + ".sav", "rb"))
     # Load diabetes datasets
     X_train, X_test, y_train, y_test = load_dataset()
 
--- mlops_tools/train.py
+++ mlops_tools/train.py
@@ -1,26 +1,33 @@
-import os
 import pickle
 import sys
+from pathlib import Path
+
+import hydra
 
 
-example_dir = os.path.dirname(os.path.realpath(__file__))
-sys.path.append(os.path.dirname(example_dir))
+mlops_tools_dir_path = Path(__file__).absolute().parent
+root_dir_path = mlops_tools_dir_path.parent
+sys.path.append(str(mlops_tools_dir_path))
+sys.path.append(str(root_dir_path.joinpath("configs")))
+
+
+from configs.config import Params  # noqa: E402
+
 from mlops_tools.models import LinReg  # noqa: E402
 from mlops_tools.utils import load_dataset, prepare_dataset  # noqa: E402
 
 
-MODEL_NAME = "basic_lin_reg"
-
-
-def main():
-    reg = LinReg(MODEL_NAME)
+@hydra.main(config_path="../configs", config_name="config", version_base="1.3")
+def main(cfg: Params):
+    model_name = cfg["model"]["name"]
+    reg = LinReg(model_name)
     # Prepare & load diabetes datasets
     prepare_dataset(0.2)
     X_train, X_test, y_train, y_test = load_dataset()
 
     # Train model & save it to disk
     reg.fit(X_train, y_train)
-    pickle.dump(reg, open(MODEL_NAME + ".sav", "wb"))
+    pickle.dump(reg, open(model_name + ".sav", "wb"))
 
 
 if __name__ == "__main__":
